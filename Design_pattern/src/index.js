86564a9ff4a6a4ae896e4c42a7651734dc019b3f
liuxilei <liuxileioo@qq.com>
2018-10-24 22:23:53 +0800

    :memo: 装饰器模式

--- Design_pattern/src/index.js
+++ Design_pattern/src/index.js
@@ -1,19 +1,22 @@
-class Adaptee {
-    specificRequest() {
-        return '德国标准的插头';
-    }
+import { deprecate } from 'core-decorators';
+
+class Person {
+    @deprecate
+    facepalm() {}
+
+    @deprecate('We stopped facepalming')
+    facepalmHard() {}
+
+    @deprecate('We stopped facepalming', { url: 'http://knowyourname.com/memes/facepalm'})
+    facepalmHarder() {}
 }
 
-class Target {
-    constructor() {
-        this.adaptee = new Adaptee();
-    }
-    request() {
-        let info = this.adaptee.specificRequest();
-        return `${info} -> 转换器 -> 中国标准的插头`;
-    }
-}
+let person = new Person();
+person.facepalm();
+//deprecate.js:31 DEPRECATION Person#facepalm: This function will be removed in future versions.
+person.facepalmHard();
+//deprecate.js:31 DEPRECATION Person#facepalmHard: We stopped facepalming
+person.facepalmHarder();
+//deprecate.js:31 DEPRECATION Person#facepalmHarder: We stopped facepalming
 
-//测试
-let target = new Target();
-console.log(target.request());
+// See http://knowyourname.com/memes/facepalm for more details.
