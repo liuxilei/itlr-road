bc07fa0e7f61f3f0045c589a7baeb026f29da442
liuxilei <liuxileioo@qq.com>
2018-10-27 12:24:42 +0800

    :memo: 代理模式

--- Design_pattern/src/index.js
+++ Design_pattern/src/index.js
@@ -1,22 +1,41 @@
-import { deprecate } from 'core-decorators';
+//明星
+let star = {
+    name: '张xx',
+    age: 25,
+    phone: 'star: 18380174854'
+};
 
-class Person {
-    @deprecate
-    facepalm() {}
+//经纪人
+let agent = new Proxy(star, {
+    get: function (target, key) {
+        if (key === 'phone') {
+            //返回经纪人自己的手机号
+            return 'aget: 18454511112';
+        }
+        if (key === 'price') {
+            //明星不报价，经纪人报价
+            return 12000
+        }
+        return target[key];
+    },
+    set: function (target, key, val) {
+        if (key === 'customPrice') {
+            if (val < 10000) {
+                //最低10w
+                throw new Error('价格太低');
+            } else {
+                target[key] = val;
+                return true;
+            }
+        }
+    }
+});
 
-    @deprecate('We stopped facepalming')
-    facepalmHard() {}
+//测试
+console.log(agent.name); //张xx
+console.log(agent.age); //25
+console.log(agent.phone); //aget: 18454511112 
+console.log(agent.price); //12000
 
-    @deprecate('We stopped facepalming', { url: 'http://knowyourname.com/memes/facepalm'})
-    facepalmHarder() {}
-}
-
-let person = new Person();
-person.facepalm();
-//deprecate.js:31 DEPRECATION Person#facepalm: This function will be removed in future versions.
-person.facepalmHard();
-//deprecate.js:31 DEPRECATION Person#facepalmHard: We stopped facepalming
-person.facepalmHarder();
-//deprecate.js:31 DEPRECATION Person#facepalmHarder: We stopped facepalming
-
-// See http://knowyourname.com/memes/facepalm for more details.
+agent.customPrice = 15000;
+console.log(agent.customPrice); //15000
